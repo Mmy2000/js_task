c2fe0611dd003af4d70aef6e1ef62fd07ab809ef
Mmy2000 <yahianada005@gmail.com>
2024-02-14 21:03:25 +0200

    finishing two tasks

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// var number = 10;
+// console.log( "the number is" + " " + number);
+
+// var x = 9;
+// if (x % 3 == 0 & x% 4 ==0) {
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
