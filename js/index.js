6274cb23c26648610fa7a23e61c43a2af9b9fe5f
Mmy2000 <yahianada005@gmail.com>
2024-02-15 17:25:05 +0200

    finishing task eleven

--- js/index.js
+++ js/index.js
@@ -112,3 +112,14 @@ for (var i = 2; i < 13; i++) {
     
 }
 */
+
+/*
+question 11
+number = Number(window.prompt("Enter the number : "))
+for (var i = 1; i < number; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+    
+}
+*/
