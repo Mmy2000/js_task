a2d30d79ac3999797938ec5b7d7107c03cdfc404
Mmy2000 <yahianada005@gmail.com>
2024-02-15 03:34:44 +0200

    finishing two tasks

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
-// var number = 10;
-// console.log( "the number is" + " " + number);
 
-// var x = 9;
-// if (x % 3 == 0 & x% 4 ==0) {
-//     console.log("yes");
-// }
-// else{
-//     console.log("no");
-// }
+/*
+ question 1 
+ x = Number(window.prompt("enter the number : "))
+ console.log(x);
+*/
+
+/*
+question 2 
+number = Number(window.prompt("Enter the number : "))
+if (number % 3 ==0 & number % 4 == 0) {
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
+*/
+
